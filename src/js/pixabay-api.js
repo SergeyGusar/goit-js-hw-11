efc231c2fffa533150069dd72cb232b5d99c8769
Sergey <110050958+SergeyGusar@users.noreply.github.com>
2023-12-16 13:16:20 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,31 +1,6 @@
 import axios from 'axios';
 
-// async function getGallery(page = 1) {
-//     const BASE_URL = 'https://pixabay.com/api/';
-//     const API_KEY = '41108429-d1297b363185a3a0442dc8f79';
-//     const q = localStorage.getItem('key');
-//     const image_type = 'photo';
-//     const orientation = 'horizontal';
-//     const safesearch = 'true';
-//     const per_page = 40;
-   
-    
-//     const queryParams = new URLSearchParams({
-//       key: API_KEY,
-//       q,
-//       image_type,
-//       page,
-//       per_page,
-//       orientation,
-//       safesearch,
-//     });
-//     try {
-//       const res = await axios.get(`${BASE_URL}?${queryParams}`);
-//       return await res.data;
-//     } catch (error) {
-//       throw new Error(error);
-//     }
-//   }
+
   const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '41108429-d1297b363185a3a0442dc8f79';
 
